346a26db8014ace705fb2d739dcc2ceb977fdaa2
beomq <beomdolq@gmail.com>
2023-06-13 00:14:30 +0900

    FEAT : bmi_calculator

--- lib/main/main_screen.dart
+++ lib/main/main_screen.dart
@@ -1,4 +1,6 @@
+import 'package:bmi_calculator/result/result_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class MainScreen extends StatefulWidget {
   const MainScreen({Key? key}) : super(key: key);
@@ -9,6 +11,39 @@ class MainScreen extends StatefulWidget {
 
 class _MainScreenState extends State<MainScreen> {
   final _formKey = GlobalKey<FormState>();
+  final _heightController = TextEditingController();
+  final _weightController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+
+    load();
+  }
+
+  @override
+  void dispose() {
+    _heightController.dispose();
+    _weightController.dispose();
+    super.dispose();
+  }
+
+  Future save() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setDouble('height', double.parse(_heightController.text));
+    await prefs.setDouble('weight', double.parse(_weightController.text));
+  }
+
+  Future load() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    final double? height = prefs.getDouble('height');
+    final double? weight = prefs.getDouble('weight');
+    if (height != null && weight != null) {
+      _heightController.text = '$height';
+      _weightController.text = '$weight';
+      print('$height, $weight');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -24,6 +59,7 @@ class _MainScreenState extends State<MainScreen> {
             crossAxisAlignment: CrossAxisAlignment.end,
             children: [
               TextFormField(
+                controller: _heightController,
                 decoration: const InputDecoration(
                   border: OutlineInputBorder(),
                   hintText: '키',
@@ -39,6 +75,7 @@ class _MainScreenState extends State<MainScreen> {
                 height: 8,
               ),
               TextFormField(
+                controller: _weightController,
                 decoration: const InputDecoration(
                   border: OutlineInputBorder(),
                   hintText: '몸무게',
@@ -55,7 +92,21 @@ class _MainScreenState extends State<MainScreen> {
               ),
               ElevatedButton(
                 onPressed: () {
-                  if (_formKey.currentState?.validate() ?? false) {}
+                  if (_formKey.currentState?.validate() == false) {
+                    return;
+                  }
+
+                  save();
+
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => ResultScreen(
+                        height: double.parse(_heightController.text),
+                        weight: double.parse(_weightController.text),
+                      ),
+                    ),
+                  );
                 },
                 child: const Text('결과'),
               ),
